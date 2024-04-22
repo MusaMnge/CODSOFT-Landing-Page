cc82c39ea13e7b74285069668ed3762042887966
MusaThando <nmngezane@gmail.com>
2024-04-22 11:26:31 +0200

    first commit

---
+++ s-script.js
@@ -0,0 +1,33 @@
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+  };
+
+  ScrollReveal().reveal(".service__container .destination-3", {
+    duration: 1000,
+    delay: 1000,
+  });
+  ScrollReveal().reveal(".service__container .destination-4", {
+    duration: 1000,
+    delay: 1500,
+  });
+  
+  ScrollReveal().reveal(".service__container .destination-5", {
+    duration: 1000,
+    delay: 1000,
+  });
+  ScrollReveal().reveal(".service__container .destination-6", {
+    duration: 1000,
+    delay: 1500,
+  });
+ 
+  ScrollReveal().reveal(".service__container .destination-7", {
+    duration: 1000,
+    delay: 1000,
+  });
+  ScrollReveal().reveal(".service__container .destination-8", {
+    duration: 1000,
+    delay: 1500,
+  });
+ 
